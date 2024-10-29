72a38ae1d53436642752f24ba0c811096b92e700
AlexandreCotorobai <alexandrecotorobai@gmail.com>
2024-10-29 10:59:00 +0000

    Debugging

--- auth/auth.py
+++ auth/auth.py
@@ -15,6 +15,8 @@ response = requests.get(
     f"https://cognito-idp.{AWS_REGION}.amazonaws.com/{USER_POOL_ID}/.well-known/jwks.json"
 )
 
+print(response.json())
+
 # Check if 'keys' field is present in the response
 if 'keys' not in response.json():
     raise ValueError("The 'keys' field is missing in the JWKS response")
